38653ac7cd86e4ef38b8e4c0ae0ac79756d9964a
amnovak0 <ana.marija.novak@supersport.hr>
2023-04-11 10:55:51 +0200

    First commit

---
+++ lib/auth_service.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'login.dart';
+import 'homepage.dart';
+
+class AuthService {
+  // GENERAL IDEA
+  
+
+
+  // 1. HANDLE AUTH STATE
+  handleAuthState() {
+    return StreamBuilder(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (BuildContext context, snapshot) {
+          if (snapshot.hasData) {
+            return const HomePage();
+          } else {
+            return const LoginPage();
+          }
+        });
+  }
+ 
+  // 2. signInWithGoogle()
+
+  signInWithGoogle() async {
+    // Trigger the authentication flow
+    final GoogleSignInAccount? googleUser = await GoogleSignIn(
+        scopes: <String>["email"]).signIn();
+
+    // Obtain the auth details from the request
+    final GoogleSignInAuthentication googleAuth = await googleUser!.authentication;
+
+    // Create a new credential
+    final credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth.accessToken,
+      idToken: googleAuth.idToken,
+    );
+
+    // Once signed in, return the UserCredential
+    return await FirebaseAuth.instance.signInWithCredential(credential);
+  }
+
+
+      // 3. HANDLE SIGNING OUT
+ signOut() {
+    FirebaseAuth.instance.signOut();
+  }
+ // determine if user is authenticated
+} 
+ 
---
+++ lib/login.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_login/auth_service.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  LoginPageState createState() => LoginPageState();
+}
+
+class LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
+
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: const Text("Google Login"),
+        backgroundColor: Colors.red.shade700,
+      ),
+      body: Container(
+        width: size.width,
+        height: size.height,
+        padding: EdgeInsets.only(
+            left: 20,
+            right: 20,
+            top: size.height * 0.2,
+            bottom: size.height * 0.5),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            const Text("Sign in with Google", style: TextStyle(fontSize: 30)),
+            GestureDetector(
+                onTap: () {
+                  AuthService().signInWithGoogle();
+                },
+                child: const Image(
+                    width: 200, image: AssetImage('assets/google.png'))),
+          ],
+        ),
+      ),
+    );
+  }
+}
